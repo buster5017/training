73b44f3f9d16a8495e3b9f3f8c7f1040907e9e52
buster5017 <buster5017@gmail.com>
2022-06-07 22:20:27 +0900

    20220607

--- banila_js/app.js
+++ banila_js/app.js
@@ -2,7 +2,12 @@ const loginInput = document.querySelector(".form input");
 const loginButton = document.querySelector(".form button");
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  if (username === "") {
+    alert("please write your name");
+  } else if (username.length > 15) {
+    alert("Your name is too long.");
+  }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
