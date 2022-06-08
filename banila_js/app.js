339321acad37e1df883fcf100a89ffa0da9789fb
buster5017 <buster5017@gmail.com>
2022-06-08 22:37:04 +0900

    banilaJS#4.5

--- banila_js/app.js
+++ banila_js/app.js
@@ -1,10 +1,16 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const link = document.querySelector("a");
+const greeting = document.querySelector(".greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  console.log(username);
+  greeting.innerText = `Hello ${username}. Welcome to the Hello World!!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
